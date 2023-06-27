b3e09ace849044cbecc8e45f631bce805a37cc84
Hasitha <hasitha123sankha@gmail.com>
2023-06-27 23:15:46 +0530

    Validation for Drugs Model

--- MNY/Model/Drug.cs
+++ MNY/Model/Drug.cs
@@ -7,13 +7,14 @@ namespace MNY.Model
        
         public int ID { get; set; }
 
-        [Required]
+        [Required(ErrorMessage ="Please Enter Trade Name")]
         public string Trde_Name { get; set; }
 
-        [Required]
+        [Required(ErrorMessage ="Please Enter Talet Generic Name")]
         public string Generic_Name { get; set; }
 
-        [Required]
+        [Required(ErrorMessage ="Please Enter Note ")]
+        [StringLength(20, MinimumLength = 10, ErrorMessage = "Noet cannot have less than 10 characters and more than 20 characters in length")]
         public string Note { get; set; }
     } 
 }
